17041c4fdaa9129f1ed77e75373d92540703a2c3
Edwin Sarver <edwin.sarver@keithley.com>
2022-01-07 15:35:25 -0500

    Get all dependencies and init.vim installing automatically with install.sh

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nvim-config
 My personal neovim configuration and installation utilities. These likely won't work for you...
+
+## Installation
+
+To install all dependencies and the neovim configuration script, run the following:
+
+```bash
+./install.sh
+```
+
+You may be prompted to enter your password for sudo.
--- install.sh
+++ install.sh
@@ -1,3 +1,65 @@
 #!/usr/bin/env bash
 
-ln -sf "$(pwd)" "$HOME/.config/nvim"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+install "$SCRIPT_DIR/init.vim" -D --target-directory="$HOME/.config/nvim"
+
+install_plug() {
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+native_packages() {
+    if dnf --version &> /dev/null; then
+        echo "Installing native packages using dnf..."
+        sudo dnf install -y \
+            gzip \
+            nodejs \
+            npm \
+            python3 \
+            python3-pip \
+            clang-tools-extra
+        echo "Installation of native packages complete!"
+    elif apt-get --version &> /dev/null; then
+        echo "apt not yet supported"
+    elif pacman --version &> /dev/null; then
+        echo "pacman not yet supported"
+    fi
+}
+
+pip_install(){
+    echo "Installing pip packages..."
+    python3 -m pip install --user \
+        neovim \
+        pyright \
+        cmake-language-server
+    echo "Installation of pip packages complete!"
+}
+
+npm_install(){
+    echo "Installing npm packages..."
+    sudo npm install -g \
+        neovim \
+        bash-language-server \
+        dockerfile-language-server-nodejs \
+        typescript-language-server \
+        vim-language-server 
+    echo "Installation of npm packages complete!"
+}
+
+rust_install(){
+    echo "Installing rust packages..."
+    if ! rustup --version; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --profile default
+        source "$HOME/.cargo/env"
+    fi
+    rustup component add rust-src
+    mkdir -p ~/.local/bin
+    curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+    chmod +x ~/.local/bin/rust-analyzer
+    echo "Installation of rust packages complete!"
+}
+
+native_packages
+npm_install
+install_plug
+pip_install
+rust_install
